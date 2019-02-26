66ac6a46917fe8ac807ddf37766ce989eeec5e72
Brian Burkard <brianburkard@gmail.com>
2019-02-25 23:02:24 -0500

    drumKeys object

---
+++ Drum Kit/app.js
@@ -0,0 +1,51 @@
+const drum = document.getElementById('drum');
+const ul = document.createElement('ul');
+const li = document.createElement('li');
+
+const drumKeys = [
+    {
+        name: 'a',
+        sound: 'clap',
+        key: 65
+    },
+    {
+        name: 's',
+        sound: 'hihat',
+        key: 83
+    },
+    {
+        name: 'd',
+        sound: 'kick',
+        key: 68
+    },
+    {
+        name: 'f',
+        sound: 'openhat',
+        key: 70
+    },
+    {
+        name: 'g',
+        sound: 'boom',
+        key: 71
+    },
+    {
+        name: 'h',
+        sound: 'ride',
+        key: 72
+    },
+    {
+        name: 'j',
+        sound: 'snare',
+        key: 74
+    },
+    {
+        name: 'k',
+        sound: 'tom',
+        key: 75
+    },
+    {
+        name: 'l',
+        sound: 'tink',
+        key: 76
+    },
+]
