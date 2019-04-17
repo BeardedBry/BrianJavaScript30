9496f269db49513ffded4364709cbda4ae8b625d
Brian Burkard <brianburkard@gmail.com>
2019-04-16 23:54:06 -0400

    playbackRate slider

--- HTML5 Video Player/scripts.js
+++ HTML5 Video Player/scripts.js
@@ -1,10 +1,14 @@
 const PLAY = document.querySelector('.player__button');
 const VIDEO = document.querySelector('.player__video');
 const VOLUME_SLIDER = document.querySelector('input[name=volume]');
+const PLAYBACK_SLIDER = document.querySelector('input[name=playbackRate]');
+
 
 PLAY.addEventListener('click',playVideo);
 VIDEO.addEventListener('click',playVideo);
 VOLUME_SLIDER.addEventListener('change', changeSound)
+PLAYBACK_SLIDER.addEventListener('change', changeSpeed)
+
 
 console.dir(VIDEO);
 
@@ -15,5 +19,10 @@ function playVideo(){
 function changeSound(e){
       //console.log(e.srcElement.value)
      VIDEO.volume = e.srcElement.value;
+}
+
+function changeSpeed(e){
+    //console.log(e.srcElement.value)
+   VIDEO.playbackRate = e.srcElement.value;
 }
 
