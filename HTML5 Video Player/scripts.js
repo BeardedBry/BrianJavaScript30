1662000c7e600daa7c607ba6becfa0abd41e83e5
Brian Burkard <brianburkard@gmail.com>
2019-04-16 23:49:31 -0400

    volume control

--- HTML5 Video Player/scripts.js
+++ HTML5 Video Player/scripts.js
@@ -1,9 +1,19 @@
 const PLAY = document.querySelector('.player__button');
 const VIDEO = document.querySelector('.player__video');
+const VOLUME_SLIDER = document.querySelector('input[name=volume]');
 
 PLAY.addEventListener('click',playVideo);
 VIDEO.addEventListener('click',playVideo);
+VOLUME_SLIDER.addEventListener('change', changeSound)
+
+console.dir(VIDEO);
 
 function playVideo(){
     VIDEO.paused == true? VIDEO.play():VIDEO.pause();
 }
+
+function changeSound(e){
+      //console.log(e.srcElement.value)
+     VIDEO.volume = e.srcElement.value;
+}
+
