74636a6058b35e016ee29caeb7cede024475b76c
Brian Burkard <brianburkard@gmail.com>
2019-04-18 21:19:25 -0400

    video skip

--- HTML5 Video Player/scripts.js
+++ HTML5 Video Player/scripts.js
@@ -1,16 +1,31 @@
-const PLAY = document.querySelector('.player__button');
+const PLAY = document.querySelector('.player__button[title="Toggle Play"]');
 const VIDEO = document.querySelector('.player__video');
 const VOLUME_SLIDER = document.querySelector('input[name=volume]');
 const PLAYBACK_SLIDER = document.querySelector('input[name=playbackRate]');
+const DATA_SKIP = document.querySelectorAll('button[data-skip]');
+const FILLED = document.querySelector('.progress__filled');
+
 
 
 PLAY.addEventListener('click',playVideo);
 VIDEO.addEventListener('click',playVideo);
-VOLUME_SLIDER.addEventListener('change', changeSound)
-PLAYBACK_SLIDER.addEventListener('change', changeSpeed)
+VOLUME_SLIDER.addEventListener('change', changeSound);
+PLAYBACK_SLIDER.addEventListener('change', changeSpeed);
 
 
-console.dir(VIDEO);
+DATA_SKIP[0].addEventListener('click', skip);
+DATA_SKIP[1].addEventListener('click', skip);
+
+
+console.dir(FILLED);
+
+function skip(e){
+    console.log(VIDEO.currentTime);
+    let skipTime = e.target.dataset.skip;
+
+    VIDEO.currentTime += Math.round(skipTime);
+
+}
 
 function playVideo(){
     VIDEO.paused == true? VIDEO.play():VIDEO.pause();
