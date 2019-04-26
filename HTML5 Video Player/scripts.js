b7d7579effa12ece2ecbd92850fd5a869a085b2b
Brian Burkard <brianburkard@gmail.com>
2019-04-26 19:44:53 -0400

    full screen html

--- HTML5 Video Player/scripts.js
+++ HTML5 Video Player/scripts.js
@@ -1,3 +1,4 @@
+
 const PLAY = document.querySelector('.player__button[title="Toggle Play"]');
 const VIDEO = document.querySelector('.player__video');
 const VOLUME_SLIDER = document.querySelector('input[name=volume]');
@@ -5,6 +6,8 @@ const PLAYBACK_SLIDER = document.querySelector('input[name=playbackRate]');
 const DATA_SKIP = document.querySelectorAll('button[data-skip]');
 const FILLED = document.querySelector('.progress__filled');
 const PROGRESS_BAR = document.querySelector('.progress');
+const TOGGLE_BUTTON = document.querySelector('.toggle');
+let mouseclicked = false;
 let progressPercent;
 
 PLAY.addEventListener('click',playVideo);
@@ -18,25 +21,19 @@ PLAYBACK_SLIDER.addEventListener('change', changeSpeed);
 DATA_SKIP[0].addEventListener('click', skip);
 DATA_SKIP[1].addEventListener('click', skip);
 
-PROGRESS_BAR.addEventListener('mousedown', scrub);
-PROGRESS_BAR.addEventListener('mouseup', scrub);
+PROGRESS_BAR.addEventListener('click', scrub);
+PROGRESS_BAR.addEventListener('mousemove', (e) => mousedown && scrub(e));
+PROGRESS_BAR.addEventListener('mousedown', () => mouseclicked = true);
+PROGRESS_BAR.addEventListener('mouseup', () => mouseclicked = false);
+document.addEventListener('mouseup', () => mouseclicked = false);
+
+
 
 
 console.dir(VIDEO);
 //clientWidth 
 function scrub(e){
-    // console.dir(e.offsetX);
-    // console.log(progressPercent);
-    // console.dir(FILLED);
     let time = (e.offsetX / VIDEO.clientWidth);
-    //FILLED.style['-webkit-flex-basis'];
-    console.dir(e);
-    console.log('client width: ' + VIDEO.clientWidth);
-    console.log('css width: ' + VIDEO.width);
-
-    console.log('offsetX: ' + e.offsetX);
-    console.log('percent: ' + time);
-
     VIDEO.currentTime = VIDEO.duration * time;
 }
 
@@ -57,6 +54,12 @@ function skip(e){
 
 function playVideo(){
     VIDEO.paused == true? VIDEO.play():VIDEO.pause();
+    updateButton()
+}
+
+function updateButton(){
+    const icon = VIDEO.paused == true? '►' : '❚❚';
+    TOGGLE_BUTTON.textContent = icon;
 }
 
 function changeSound(e){
